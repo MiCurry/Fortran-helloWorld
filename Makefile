7d6123f9c857913d5436cb4789a7a575722b62b2
Miles A. Curry <currymiles@gmail.com>
2020-08-29 11:13:52 -0600

    init commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	gfortran -o helloWorld helloWorld.f90
+
+clean:
+	rm -f helloWorld
---
+++ helloWorld.f90
@@ -0,0 +1,8 @@
+program helloWorld
+
+   implicit none
+
+   write(0,*) "Hello World!"
+   stop 0
+
+end program helloWorld
